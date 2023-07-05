8642f20e384406ee14cae48bb71eada9f83c1052
RenanGava <renandellecrodegava7040@gmail.com>
2023-07-04 21:36:24 -0300

    finish login page

--- web-app/src/app/page.tsx
+++ web-app/src/app/page.tsx
@@ -1,10 +1,13 @@
 import { Header } from '@/components/Header'
 import styles from './page.module.scss'
+import { FormLogin } from '@/components/FormLogin'
 
 export default function Home() {
   return (
     <main className={styles.container}>
       <Header/>
+      <FormLogin/>
     </main>
   )
 }
+
---
+++ web-app/src/components/FormLogin/index.tsx
@@ -0,0 +1,72 @@
+"use client"
+import { ChangeEvent, FormEvent, useState } from 'react'
+import styles from './styles.module.scss'
+import Image from 'next/image'
+import iconPassword from '../../icons/password.png'
+import iconEmail from '../../icons/email.png'
+
+
+
+export function FormLogin() {
+
+    const [email, setEmail] = useState<string | null>(null)
+    const [password, setPassword] = useState<string | null>(null)
+
+
+    const labelEmail = !email && (
+        <label htmlFor="email">
+            <Image
+                className={styles.icon}
+                src={iconEmail}
+                alt=''
+            />
+            Email
+        </label>
+    )
+
+    const labelPassword = !password && (
+        <label htmlFor="email">
+            <Image
+                className={styles.iconPassword}
+                src={iconPassword}
+                alt=''
+            />
+            Senha
+        </label>
+    )
+
+
+    async function handleLogin(event: FormEvent){
+        event.preventDefault()
+        console.log(event);
+    }
+
+    return (
+        <form
+            className={styles.formLogin}
+            onSubmit={handleLogin}
+        >
+            <div className={styles.inputfield}>
+
+                {labelEmail}
+
+                <input
+                    onChange={e => setEmail(e.target.value)}
+                    type='text'
+                    id="email"
+                />
+            </div>
+            <div className={styles.inputfield}>
+                {labelPassword}
+
+                <input
+                    onChange={e => setPassword(e.target.value)}
+                    id="password"
+                    type={password ? 'password' : 'text'}
+                />
+            </div>
+
+            <button type='submit'>GET STARTED</button>
+        </form>
+    )
+}
