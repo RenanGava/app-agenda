bdc4c66979d051abde8e19b00f939c648700f4c7
RenanGava <renandellecrodegava7040@gmail.com>
2023-07-27 20:46:40 -0300

    init dev server

---
+++ server/entrypoint.sh
@@ -0,0 +1,5 @@
+yarn install
+# yarn prisma generate
+# yarn prisma migrate deploy
+yarn build
+yarn start
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,19 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 import cors from 'cors'
+import { config } from 'dotenv'
 
+config()
 const app = express()
 
 app.use(express.json())
-app.use(cors)
+app.use(cors())
 
+app.get("/", (req: Request, res: Response) => {
 
+    return res.json({ok: true})
+})
 
-
-app.listen()
+// process.env.PORT || 
+app.listen(process.env.PORT || 3333, () => {
+    console.log('Server Rodando!', process.env.PORT);
+})
